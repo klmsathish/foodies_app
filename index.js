befe45c2fdf2d434d12c0e5dc851fc1dc76a91e3
M.SathishKumar <54014069+klmsathish@users.noreply.github.com>
2021-05-18 23:49:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -388,5 +388,6 @@ app.post('/api',async (req, res) => {
     res.send({" Status":"ok"});
 })
 
-
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
